f3e4fd1e15278935592ece8748679aa42ca2e0c9
Jaime Irazabal <jaimeirazabal1@gmail.com>
2016-12-20 09:05:13 -0400

    agregando cotizaciones

--- default/app/controllers/cotizacion_controller.php
+++ default/app/controllers/cotizacion_controller.php
@@ -3,11 +3,11 @@
 class CotizacionController extends AppController{
 
 	public function index(){
-
+		$this->cotizaciones = array();
 	}
 
 	public function create(){
-
+		$this->productos = Load::model('producto')->find("conditions: user_id='".Auth::get('id')."'");
 	}
 
 	public function edit(){
